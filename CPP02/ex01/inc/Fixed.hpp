f68a06dead1e294a1415d80f9056c34d58b65d45
Leo Agea <leoagea@gmail.com>
2024-09-16 23:18:45 +0200

    Update : ex01 files added

---
+++ CPP02/ex01/inc/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 19:55:01 by lagea             #+#    #+#             */
+/*   Updated: 2024/09/16 23:18:14 by lagea            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __HARL_HPP__
+#define __HARL_HPP__
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class Fixed
+{
+    public:
+        Fixed();
+        Fixed(const int d);
+        Fixed(const float f);
+        Fixed(const Fixed &t);
+        Fixed &operator=(const Fixed &t);
+        ~Fixed();
+        
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        float toFloat( void ) const;
+        int toInt( void ) const;
+        
+    private:
+        int _fixedPointNumber;
+        static const int _fractionalBits = 8;
+    
+};
+
+#endif
