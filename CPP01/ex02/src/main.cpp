8a3ba83ee5c6e3e0f12d70d1741aa9ccd14442be
leoagea <leoagea@gmail.com>
2024-09-17 13:22:11 +0200

    Update : change var name to correspond to the subject

--- CPP01/ex02/src/main.cpp
+++ CPP01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lagea <lagea@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/15 03:24:19 by lagea             #+#    #+#             */
-/*   Updated: 2024/09/16 18:21:29 by lagea            ###   ########.fr       */
+/*   Updated: 2024/09/17 13:20:47 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,16 @@
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
-    std::string *ptr = &str;
-    std::string &ref = str;
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
 
-    std::cout << "Memory address of the string : " << &str << std::endl;
-    std::cout << "Memory address held by ptr   : " << ptr << std::endl;
-    std::cout << "Memory address held by ref   : " << &ref << std::endl;
-    
+    std::cout << "Memory address of the string       : " << &str << std::endl;
+    std::cout << "Memory address held by stringPTR   : " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF   : " << &stringREF << std::endl;
+
     std::cout << std::endl;
     
-    std::cout << "Value of the string  : " << str << std::endl;
-    std::cout << "Value pointed by ptr : " << *ptr << std::endl;
-    std::cout << "Value pointed by ref : " << ref << std::endl;
+    std::cout << "Value of the string        : " << str << std::endl;
+    std::cout << "Value pointed by stringPTR : " << *stringPTR << std::endl;
+    std::cout << "Value pointed by stringREF : " << stringREF << std::endl;
 }
