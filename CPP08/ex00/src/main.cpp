985e272572a24d88b61859a210768b43393a413e
Leo AGEA <lagea@student.s19.be>
2024-11-06 14:56:21 +0100

    Update : add tests

--- CPP08/ex00/src/main.cpp
+++ CPP08/ex00/src/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lagea < lagea@student.s19.be >             +#+  +:+       +#+        */
+/*   By: lagea <lagea@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 23:00:13 by lagea             #+#    #+#             */
-/*   Updated: 2024/11/03 23:50:17 by lagea            ###   ########.fr       */
+/*   Updated: 2024/11/06 13:00:27 by lagea            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
 
 int main()
 {
 	{
 		std::vector<int> vec;
-		for (int i=0; i < 15; i++)
+		for (int i=0; i < 10; i++)
 			vec.push_back(i);
 
 		try
 		{
 			std::cout << *easyfind(vec, 5) << std::endl;
-			// std::cout << *easyfind(vec, 20) << std::endl;
+			std::cout << *easyfind(vec, 20) << std::endl;
 		}
 		catch(const std::exception& e)
 		{
@@ -38,6 +40,38 @@ int main()
 		std::cout << std::endl;
 	
 	{
-		
+		std::list<int> lst;
+		for (int i=0; i < 10; i++)
+			lst.push_back(i);
+
+		try
+		{
+			std::cout << *easyfind(lst, 5) << std::endl;
+			std::cout << *easyfind(lst, 20) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
+		std::cout << std::endl;
+		std::cout << "============================" << std::endl;
+		std::cout << std::endl;
+	
+	{
+		std::deque<int> queue;
+		for (int i=0; i < 10; i++)
+			queue.push_back(i);
+
+		try
+		{
+			std::cout << *easyfind(queue, 5) << std::endl;
+			std::cout << *easyfind(queue, 20) << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
 	}
 }
